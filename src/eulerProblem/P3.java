70c6d41432207eda4955ee8389546da7734e8ec9
fmudhir <fmudhir@gmail.com>
2019-07-28 00:34:29 -0500

    p3 solved

--- src/eulerProblem/P3.java
+++ src/eulerProblem/P3.java
@@ -1,70 +1,58 @@
 package eulerProblem;
 
-import java.lang.Math; 
-
-
 public class P3 {
-//	private static long query = 21L;
-//	private static long query = 13195L;
-//	private static long query = 6008514L; 
 
-//	private static long query = 6008514521L; 	// best: 13030
-	private static long query = 600851475143L;
-	private static long halfQuery = query / 2L;
-
+	private static long min = 2L;
+	private static long prime = 600851475143L;
 
 	public static void main(String[] args) {
-		long start = System.currentTimeMillis();
-		System.out.println("result:");
-		long [] primes = new long[1000];
 
-		long largestFactorPrime = 1L;
-		for (long i = halfQuery + 1; i > 1L; i = i - 2) {
-			if (query % i == 0L && isPrime(i, primes)) {
-				largestFactorPrime = i;
+		long factor = nextSmallerFactor();
+		while (factor > 1L) {
+			if (primeNumber(factor)) {
+				System.out.println("solution: ");
+				System.out.println(factor);
 				break;
-			}
-		}
-
-		System.out.println(largestFactorPrime);
-		System.out.println("ms: " + (System.currentTimeMillis() - start));
-	}
-	
-	private static boolean isPrime(long x, long [] primes) {
-
-		boolean isPrime = true;
-		if (x == 1L) {
-			isPrime = true;
-		} else if (x == 2L) {
-			isPrime = false;
-		} else if (x % 2L == 0) {
-			isPrime = false;
-		} else if (x % 3L == 0 && x != 3L) {
-			isPrime = false;
-		} else if (x % 5L == 0 && x != 5L) {
-			isPrime = false;
-		} else {
-			for (int i = 0; i < primes.length && primes[i] != 0; i++) {
-				if (x % primes[i] == 0) {
-					isPrime = false;
-					break;
-				}
-			}
-			if (isPrime) {
-				for (long i = 3L; i < (long) Math.sqrt(x); i = i + 2) {
-					if (x % i == 0L) {
-						isPrime = false;
-						break;
-					}
-				}		
+			} else {
+				factor = nextSmallerFactor();
 			}
 		}
 		
-		if  (isPrime)
-			primes[primes.length - 1] = x;
 
-		return isPrime;
 	}
 
-}
+	private static boolean primeNumber(long candidate) {
+		long i;
 
+		if (candidate % 2L == 0L) {
+			return false;
+		} else if (candidate != 3L && candidate % 3L == 0L) {
+			return false;
+		} else if (candidate != 5L && candidate % 5L == 0L) {
+			return false;
+		}
+
+		long half = candidate / 2L;
+		for (i = 7L; i < half; i++) {
+			if (i % 2L == 0L) {
+				continue; 
+			} else if (i % 3L == 0L) {
+				continue;
+			} else if (i % 5L == 0L) {
+				continue;
+			} else if (candidate % i == 0L) {
+				return false;
+			}
+		}
+
+		return i == half;
+
+	}
+
+	private static long nextSmallerFactor() {
+		long i;
+		for (i = min; prime % i != 0 ;i++) {}
+		min = i + 1L;
+		return prime / i;
+	}
+}
